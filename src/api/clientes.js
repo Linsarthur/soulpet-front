86d0ccbea988d19d743379d9d02d8be016db3fbe
Linsarthur <linsarthur02@gmail.com>
2024-08-07 15:09:11 -0300

    ultimo projeto antes do projeto final

---
+++ src/api/clientes.js
@@ -0,0 +1,35 @@
+//Este arquivo possui funções para realizar as operações do CRUD de clientes
+
+import axios from "axios";
+
+export async function getClientes() {
+  const response = await axios.get("http://localhost:3000/clientes");
+
+  //dentro de data está o json de resposta do back-end
+  return response.data;
+}
+
+export async function addCliente(data) {
+  //o 2° parâmetro do post é o corpo da requisição
+  const response = await axios.post("http://localhost:3000/clientes", data);
+
+  return response.data;
+}
+
+export async function deleteCliente(id) {
+  const response = await axios.delete(`http://localhost:3000/clientes/${id}`);
+  return response.data;
+}
+
+export async function getCliente(id) {
+  const response = await axios.get(`http://localhost:3000/clientes/${id}`);
+  return response.data;
+}
+
+export async function updateCliente(id, data) {
+  const response = await axios.put(
+    `http://localhost:3000/clientes/${id}`,
+    data
+  );
+  return response.data;
+}
